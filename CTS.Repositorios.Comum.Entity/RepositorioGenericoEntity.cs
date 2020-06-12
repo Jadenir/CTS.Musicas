96db96fe958ff21ae4d435c6ab221f7a20e34e0a
Jadenir <jadenir.p.s@gmail.com>
2020-06-11 21:29:54 -0300

    Implementando Repository Pattern, classe generica com Entity Framework

---
+++ CTS.Repositorios.Comum.Entity/RepositorioGenericoEntity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTS.Repositorios.Comum.Entity
+{
+    public class RepositorioGenericoEntity<TEntidade, TChave> : IRepositorioGenerico<TEntidade, TChave>
+        where TEntidade : class
+    {
+        private DbContext _contexto;
+
+        public RepositorioGenericoEntity(DbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Alterar(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Attach(entidade);
+            _contexto.Entry(entidade).State = EntityState.Modified;
+            _contexto.SaveChanges();
+        }
+
+        public void Excluir(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Attach(entidade);
+            _contexto.Entry(entidade).State = EntityState.Deleted;
+            _contexto.SaveChanges();
+        }
+
+        public void ExcluirPorId(TChave id)
+        {
+            TEntidade entidade = SelecionarPorId(id);
+            Excluir(entidade);
+        }
+
+        public void Inserir(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Add(entidade);
+            _contexto.SaveChanges();
+        }
+
+        public List<TEntidade> Selecionar()
+        {
+            return _contexto.Set<TEntidade>().ToList();
+        }
+
+        public TEntidade SelecionarPorId(TChave id)
+        {
+            return _contexto.Set<TEntidade>().Find(id);
+        }
+    }
+}
