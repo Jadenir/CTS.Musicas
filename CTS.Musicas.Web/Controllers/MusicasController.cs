c5a4af4eca24bc76129fe75c0f6ddc2c96b1e8dc
Jadenir <jadenir.p.s@gmail.com>
2020-06-14 19:53:55 -0300

    Adiciona controller e as views necessarias usando o Scalffdinf do Entity

---
+++ CTS.Musicas.Web/Controllers/MusicasController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CTS.Musicas.AcessoDados.Entity.Context;
+using CTS.Musicas.Dominio;
+
+namespace CTS.Musicas.Web.Controllers
+{
+    public class MusicasController : Controller
+    {
+        private MusicasDbContext db = new MusicasDbContext();
+
+        // GET: Musicas
+        public ActionResult Index()
+        {
+            var musicas = db.Musicas.Include(m => m.Album);
+            return View(musicas.ToList());
+        }
+
+        // GET: Musicas/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musica);
+        }
+
+        // GET: Musicas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAlbum = new SelectList(db.Albuns, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Musicas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,IdAlbum")] Musica musica)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Musicas.Add(musica);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAlbum = new SelectList(db.Albuns, "Id", "Nome", musica.IdAlbum);
+            return View(musica);
+        }
+
+        // GET: Musicas/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAlbum = new SelectList(db.Albuns, "Id", "Nome", musica.IdAlbum);
+            return View(musica);
+        }
+
+        // POST: Musicas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,IdAlbum")] Musica musica)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(musica).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAlbum = new SelectList(db.Albuns, "Id", "Nome", musica.IdAlbum);
+            return View(musica);
+        }
+
+        // GET: Musicas/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musica);
+        }
+
+        // POST: Musicas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Musica musica = db.Musicas.Find(id);
+            db.Musicas.Remove(musica);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
