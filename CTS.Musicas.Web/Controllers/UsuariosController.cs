3b510152815ed154ca2618ed841d1ac33042c1a7
Jadenir <jadenir.p.s@gmail.com>
2020-06-16 21:53:32 -0300

    Iniciando autenticação com Identity

---
+++ CTS.Musicas.Web/Controllers/UsuariosController.cs
@@ -0,0 +1,80 @@
+﻿using CTS.Musicas.AcessoDados.Entity.Context;
+using CTS.Musicas.Web.Identity;
+using CTS.Musicas.Web.ViewModels.Usuario;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace CTS.Musicas.Web.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public ActionResult CriarUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CriarUsuario(UsuarioViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var userStore = new UserStore<IdentityUser>(new MusicasIdentityDbContext());
+                var userManager = new UserManager<IdentityUser>(userStore);
+                var identityUser = new IdentityUser
+                {
+                    UserName = viewModel.Email,
+                    Email = viewModel.Email
+                };
+                IdentityResult resultado = userManager.Create(identityUser, viewModel.Senha);
+                if (resultado.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("erro_identity", resultado.Errors.First());
+                    return View(viewModel);
+                }
+            }
+            return View(viewModel);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UsuarioViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var userStore = new UserStore<IdentityUser>(new MusicasIdentityDbContext());
+                var userManager = new UserManager<IdentityUser>(userStore);
+                var usuario = userManager.Find(viewModel.Email, viewModel.Senha);
+                if (usuario == null)
+                {
+                    ModelState.AddModelError("erro_identity", "Usuário ou Senha incorretos.");
+                    return View(viewModel);
+                }
+                var authManager = HttpContext.GetOwinContext().Authentication;
+                var identity = userManager.CreateIdentity(usuario, DefaultAuthenticationTypes.ApplicationCookie);
+                authManager.SignIn(new Microsoft.Owin.Security.AuthenticationProperties
+                {
+                    IsPersistent = false
+                }, identity);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ CTS.Musicas.Web/ViewModels/Usuario/UsuarioViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CTS.Musicas.Web.ViewModels.Usuario
+{
+    public class UsuarioViewModel
+    {
+        [Required(ErrorMessage ="O e-mail é obrigatório")]
+        [MaxLength(50,ErrorMessage ="O e-mail não pode ter mas que 30 caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="A senha é obrigratória")]
+        [MaxLength(20, ErrorMessage = "A senha não pode ter mas que 20 caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
